64231d280c8d7bdacfeac768bc687572469095ce
AntoPortelance <anto.portelance@gmail.com>
2025-03-06 23:26:43 +0100

    Add files via upload

--- V1/UDP.ino
+++ V1/UDP.ino
@@ -35,45 +35,53 @@ void loop() {
   receivePacket();
 }
 
-void reply(int tracker, byte* index) {
-    
-    if tracker) {
-        case 0: // Confirmation
-          framesize = index.sizeof() + 4;
-          byte frame[10] = concatenateArrays({0xEE, 0xEE,0XEE,0xEE},index )
-          udp.send(senderIP, senderPort, message, sizeof(message)); 
-        case 4: // IHM
-            Serial.println("IHM control");
-            break;
-        
-        case 5: // Start test
-            Serial.println("Starting test");
-            break;
-        
-        case 6: // Confirm test
-            Serial.println("Test confirmed");
-            break;
-        
-        case 7: // Abort test
-            Serial.println("Test aborted");
-            break;
-        
-        case 8: // Send data
-            Serial.println("Sending data");
-            break;
-        
-        case 9: // Copy of command
-            Serial.println("Copying command");
-            break;
-        
-        case 10: // Warning 1
-            Serial.println("Warning 1 triggered");
-            break;
-        
-        case 11: // Warning 2
-            Serial.println("Warning 2 triggered");
-            break;
-    }
+void reply(int tracker, byte* index, int size) {
+    if (tracker == 0){
+      byte message[size+4];
+      message[0] = 0xEE;
+      message[1] = 0xEE;
+      message[2] = 0xEE;
+      message[3] = 0xEE;
+      for (int j = 0; j < size; j++) { // copy data
+      message[j+4] = index[j]; } 
+      udp.send(senderIP, senderPort, message, sizeof(message));}
+    else if(tracker == 1) { // data
+      byte message[size+4]; 
+      message[0] = 0xFF;
+      message[1] = 0xFF;
+      message[2] = 0xFF;
+      message[3] = 0xFF;
+      for (int j = 0; j < size; j++) {message[j+4] = index[j]; }  // copy data 
+      udp.send(senderIP, senderPort, message, sizeof(message));}
+    else if (tracker == 2) { // Start test
+      byte message[size+4]; 
+      message[0] = 0xDD;
+      message[1] = 0xDD;
+      message[2] = 0xDD;
+      message[3] = 0xDD;
+      for (int j = 0; j < size; j++) { // copy data
+      message[j+4] = index[j]; } 
+      udp.send(senderIP, senderPort, message, sizeof(message));}
+    else if (tracker == 3) { // Confirm test
+        Serial.println("Test confirmed");} 
+    else if (tracker == 4) { // Abort test
+      byte message[size+4]; 
+      message[0] = 0xCC;
+      message[1] = 0xCC;
+      message[2] = 0xCC;
+      message[3] = 0xCC;
+      for (int j = 0; j < size; j++) { // copy data
+      message[j+4] = index[j]; } 
+      udp.send(senderIP, senderPort, message, sizeof(message));}
+    else if (tracker == 5) { // Abort test
+      byte message[size+4]; 
+      message[0] = 0xBB;
+      message[1] = 0xBB;
+      message[2] = 0xBB;
+      message[3] = 0xBB;
+      for (int j = 0; j < size; j++) { // copy data
+      message[j+4] = index[j]; } 
+      udp.send(senderIP, senderPort, message, sizeof(message));}
 }
 
 void receivePacket() {
@@ -94,37 +102,11 @@ void receivePacket() {
   }
 
   if (instructions[0] == 0xFF  && instructions[1] == 0xFF && instructions[2] == 0xFF && instructions[3] == 0xFF){ // Valve
-
-    switch (instructions[5]) {
-      case 0x00: // SV11
-      case 0x01: // SV12
-      case 0x02: // SV13
-      case 0x03: // SV21
-      case 0x04: // SV22
-      case 0x05: // SV24
-      case 0x06: // SV31
-      case 0x07: // SV32
-      case 0x08: // SV33
-      case 0x09: // SV34
-      case 0x10: // SV35
-      case 0x11: // SV36
-      case 0x12: // SV51
-      case 0x13: // SV52
-      case 0x14: // SV53
-      case 0x15: // SV61
-      case 0x16: // SV62
-      case 0x17: // SV63
-      case 0x18: // SVXX
-        if (instructions[6] == 0x00 || instructions[6] == 0x01) {
-          //setValve(req[j], req[j + 1]);  // Activer ou désactiver la valve
-          // byte index[2] = {instructions[5],instructions[6]};
-          reply(0,instructions);
-        }
-        break;
+    if (instructions[6] == 0x00 || instructions[6] == 0x01) {
+      //setValve(instructions[5], instructions[6]);  // Activer ou désactiver la valve
     }
   }
   if (instructions[0] == 0xFF  && instructions[1] == 0xFF && instructions[2] == 0xEE && instructions[3] == 0xEE){ // bang-bang
-
   }
   if (instructions[0] == 0xEE  && instructions[1] == 0xEE && instructions[2] == 0xEE && instructions[3] == 0xEE){ // Actuators
 
@@ -148,14 +130,3 @@ void receivePacket() {
   printf("\n\r");
 }
 
-void concatenateArrays(int* arr1, int size1, int* arr2, int size2, int* result) {
-  // Copy elements from the first array
-  for (int i = 0; i < size1; i++) {
-    result[i] = arr1[i];
-  }
-
-  // Copy elements from the second array
-  for (int i = 0; i < size2; i++) {
-    result[size1 + i] = arr2[i];
-  }
-}
