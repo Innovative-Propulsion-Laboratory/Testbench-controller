1f27fdfb6446897350d74db55847a59a7fff98d5
AntoPortelance <anto.portelance@gmail.com>
2025-03-27 00:35:30 +0100

    sequence db

--- V1/main/sequence.cpp
+++ V1/main/sequence.cpp
@@ -8,6 +8,7 @@ uint16_t Purge_duration = 1000;
 uint16_t Chilldown_on_duration = 1000;
 uint32_t Chilldown_off_duration = 1000;
 float chill_temp = 270; 
+uint16_t Chilldown_verifed_duration = 100;
 uint32_t Maw_chilldown_duration = 10000;
 uint32_t Chilldown_to_cooling = 1000;
 float colling_pressure = 3000;
@@ -28,7 +29,7 @@ uint16_t Cooling_duration_after_end_burn = 2000;
 
 uint16_t Confirm_to_purge_delay;
 
-uint16_t Chilldown_end;
+uint16_t Chilldown_finished;
 
 uint16_t PS63_duration;
 uint16_t PS63_seems_rise;
@@ -54,7 +55,7 @@ void Sequence(){ // TODO: add all the arguments of the function
     
     uint32_t T_confirm = millis();
     uint32_t Chilldown_duration;
-    uint32_t Chilldown_start;_step = "test";
+    active = "test";
     test_step = 1;
     do {
         sensorsLoop();
@@ -81,13 +82,10 @@ void Sequence(){ // TODO: add all the arguments of the function
                 }
             }
             case 4 { // check of end chilldown
-                if (millis() >= (T_confirm+Confirm_to_purge_delay+Purge_duration+Chilldown_on_duration+Chilldown_off_duration)) {
-                    if (chill_temp >= Data.TS11 ){ 
-                        check_chill_temp = true;
-                        Chilldown_end = millis();
-                        Chilldown_duration = Chilldown_end - Chilldown_start;
-                        test = 1;
-                        test_step++;
+                if (millis() <= (T_confirm+Confirm_to_purge_delay+Purge_duration+Chilldown_on_duration+Chilldown_off_duration)) {
+                    if (chill_temp <= Data.TS12 ){ 
+                        chill_temp_seems_ok = millis();
+                        test_step = test_step + 0.5;
                         // maybe send a message chilldown okay
                     } 
                     else if ((millis() - Chilldown_start) >= Max_chilldown_duration){
@@ -96,10 +94,22 @@ void Sequence(){ // TODO: add all the arguments of the function
                         // return to the activ_step
                     }
                     else {
-                        T_confirm = millis()+Confirm_to_purge_delay+Purge_duration;
+                        T_confirm = millis();
                         test_step = 2;
                     }
                 }
+            }
+            case 4.5{
+                if (Data.TS12 >= chill_temp) && (chill_temp_seems_ok >= Chilldown_verifed_duration){
+                    Chilldown_finished = millis();
+                    Chilldown_duration = Chilldown_finished - Chilldown_start;
+                    test = 1;
+                    test_state = test_state + 0.5;
+                }
+                else if (Data.TS41 < chill_temp){
+                    test_state = 4;
+                }
+                
             }
             case 5{// open water coolling
                 if ( millis() >= (Chilldown_finished+Chilldown_to_cooling)){
@@ -149,7 +159,7 @@ void Sequence(){ // TODO: add all the arguments of the function
                     // couper alimentation igneter 
                     test_state++;
                     T0 = millis();
-                    setValve(SV24,0);
+                    setValve(SV24,1);
                 }
                 else if (/*allumeur eteins*/){
                     test_state = 9;
@@ -158,7 +168,7 @@ void Sequence(){ // TODO: add all the arguments of the function
             case 11 { // open bypass LOX
                 if (millis() >= (T0 + ETH_to_LOX_bypass)){
                     Bypass_duration = millis();
-                    setValve(SV13,0);
+                    setValve(SV13,1);
                     test_state++;
                 }
             }
@@ -177,7 +187,7 @@ void Sequence(){ // TODO: add all the arguments of the function
             case 13 { // check if ETH rise the pressure during as time
                 if (Data.PS41 >= Bypass_pressure) && (Data.PS42 >= Bypass_pressure) && (Bypass_seems_rise >= Bypass_verifed_duration){
                     ETH_open = millis();
-                    setValve(SV22,0);
+                    setValve(SV22,1);
                     test_state++;
                 }
                 else if (Data.PS41 < Bypass_pressure) or (Data.PS42 < Bypass_pressure){
@@ -187,7 +197,7 @@ void Sequence(){ // TODO: add all the arguments of the function
             case 14 {
                 if (millis()>= (T0+ETH_to_LOX_main)){
                     Main_duration = millis();
-                    setValve(SV22,1);
+                    setValve(SV12,1);
                     test_state++;
                 }
             }
@@ -213,8 +223,8 @@ void Sequence(){ // TODO: add all the arguments of the function
                 }
             }
             case 17 {
-                setValve(SV24,1);
-                setValve(SV13,1);
+                setValve(SV24,0);
+                setValve(SV13,0);
                 Nominal_pressure_reached = millis();
                 state_state++;
                 
@@ -226,35 +236,32 @@ void Sequence(){ // TODO: add all the arguments of the function
             }
             case 19{
                 if (T0 + burn_duration){
-                    setValve(SV12,1);
-                    setValve(SV36,0);
+                    setValve(SV12,0);
+                    setValve(SV36,1);
                     test_state++;
                 }
             }
             case 20{
                 if (T0 + burn_duration + LOX_to_ETH_closing_delay){
-                    setValve(SV22,1);
-                    setValve(SV35,0);
+                    setValve(SV22,0);
+                    setValve(SV35,1);
                     test_state++;
                 }
             }
             case 21{
                 if (T0 + burn_duration + LOX_to_ETH_closing_delay + Purge_duration){
-                    setValve(SV36,1);
-                    setValve(SV35,1);
+                    setValve(SV36,0);
+                    setValve(SV35,0);
                     test_state++;
                 }
             }
             case 22{
                 if (T0 + burn_duration + LOX_to_ETH_closing_delay + Purge_duration + Cooling_duration_after_end_burn){
-                    setValve(SV63,1);
+                    setValve(SV63,0);
                     state = "active";
                 }
             }
-            // add more cases for the next steps
-            // change the variabl_step to "active" and test_step back to 0 during the last step 
         }
     }while (state = "test")
-    // j'ai changé les conditions du coup mais je te laisse ça là au cas où(check_chill_temp == false) or (Chilldown_duration == Max_chilldown_duration);
 
 }
