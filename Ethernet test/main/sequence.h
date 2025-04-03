46b6be3e690d5f83dfe608633a3af31b53dd6225
AntoPortelance <anto.portelance@gmail.com>
2025-04-03 17:39:39 +0200

    db

--- Ethernet test/main/sequence.h
+++ Ethernet test/main/sequence.h
@@ -4,20 +4,23 @@
 #include <stdint.h>  
 
 struct __attribute__((packed)) sequence_data {
+    bool cooling_enable;
+    uint16_t tvc_pattern;   
     uint16_t Confirm_to_purge_delay;
-    uint16_t Purge_duration;
+    uint16_t Purge_duration1;
     uint16_t Chilldown_on_duration;
-    uint32_t Chilldown_off_duration;
+    uint16_t Chilldown_off_duration;
     float chill_temp;
     uint16_t Chilldown_verified_duration;
-    uint32_t Max_chilldown;
-    uint32_t Chilldown_to_cooling;
+    uint16_t Max_chilldown;
+    uint16_t Chilldown_to_cooling;
     float cooling_pressure;
-    uint32_t PS63_check_duration;
+    uint16_t PS63_check_duration;
     uint16_t PS63_verified_duration;
     uint16_t Ign_check_duration;
     uint16_t Ign_verified_duration;
     uint16_t ETH_to_LOX_bypass;
+    uint16_t ETH_to_LOX_main;
     float Bypass_pressure;
     uint16_t Bypass_check_duration;
     uint16_t Bypass_verified_duration;
@@ -25,9 +28,13 @@ struct __attribute__((packed)) sequence_data {
     uint16_t Main_check_duration;
     uint16_t Main_verified_duration;
     uint16_t burn_duration;
+    uint16_t TVC_pattern_duration;
     uint16_t LOX_to_ETH_closing_delay;
+    uint16_t Purge_duration2;
     uint16_t Cooling_duration_after_end_burn;
 };
+
+extern sequence_data Sequence_data;
 
 void Sequence();
 void set_offset_pressure();
